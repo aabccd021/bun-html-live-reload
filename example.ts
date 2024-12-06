de8b386fec6eaf49c9a49fb8e8e9ce89b1150de2
aabccd021 <aabccd021@gmail.com>
2024-12-06 13:15:54 +0700

    Refactor: Simplify test command in deploy and test workflows, delete unused files.

--- example.ts
+++
@@ -1,9 +0,0 @@
-import { withHtmlLiveReload } from "./index.ts";
-
-Bun.serve({
-  fetch: withHtmlLiveReload(async () => {
-    return new Response("<div>Init</div>", {
-      headers: { "Content-Type": "text/html" },
-    });
-  }),
-});
---
+++ reload.test.ts
@@ -0,0 +1,63 @@
+import { chromium } from "playwright";
+import * as fs from "fs";
+import { afterEach, expect, test } from "bun:test";
+
+const serverCodeInit = `
+import { withHtmlLiveReload } from "./bun-html-live-reload.ts";
+
+Bun.serve({
+  fetch: withHtmlLiveReload(async () => {
+    return new Response("<div>Init</div>", {
+      headers: { "Content-Type": "text/html" },
+    });
+  }),
+});
+`;
+
+const serverCodeChanged = `
+import { withHtmlLiveReload } from "./bun-html-live-reload.ts";
+Bun.serve({
+  fetch: withHtmlLiveReload(async () => {
+    return new Response("<div>Changed</div>", {
+      headers: { "Content-Type": "text/html" },
+    });
+  }),
+});
+`;
+
+let close: () => Promise<void> | undefined;
+
+afterEach(async () => {
+  await close?.();
+})
+
+test("hot reload works", async () => {
+  const systemTmp = process.env["TMPDIR"] ?? "/tmp";
+  const tmpdir = fs.mkdtempSync(`${systemTmp}/bun-`);
+  const serverPath = `${tmpdir}/server.ts`;
+  const libPath = `${tmpdir}/bun-html-live-reload.ts`;
+
+  await Bun.write(serverPath, serverCodeInit);
+
+  fs.copyFileSync(`${import.meta.dir}/index.ts`, libPath);
+
+  const child = Bun.spawn(["bun", "--hot", serverPath], { stderr: "ignore" });
+
+  const browser = await chromium.launch();
+
+  close = async () => {
+    child?.kill();
+    await browser.close();
+  };
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("http://localhost:3000");
+
+  expect(await page.innerText("div")).toBe("Init");
+
+  await Bun.write(serverPath, serverCodeChanged);
+  await page.waitForEvent("framenavigated");
+
+  expect(await page.innerText("div")).toBe("Changed");
+});
